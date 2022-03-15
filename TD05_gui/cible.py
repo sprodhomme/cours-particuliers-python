4c1f3514213714a9578c5e35e3fb4e097071a7b9
Thomas <91213934+uvsq22104079@users.noreply.github.com>
2022-03-15 20:39:57 +0100

    Update cible.py

--- TD05_gui/cible.py
+++ TD05_gui/cible.py
@@ -1,7 +1,16 @@
 import tkinter as tk
 
-CANVAS_WIDTH, CANVAS_HEIGHT = 600, 400
+CANVAS_WIDTH, CANVAS_HEIGHT = 400, 400
 
 root = tk.Tk()
 
-canvas = tk.Canvas(root, width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
+canvas = tk.Canvas(root, width = CANVAS_WIDTH, height = CANVAS_HEIGHT, bg = "#000000")
+
+x = 200
+y= 200
+
+
+canvas.create_oval( x, y, x+100, y+100, bg="blue", width=15)
+
+canvas.grid()
+root.mainloop()
