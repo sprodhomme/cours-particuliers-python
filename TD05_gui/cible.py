c4b664804919bd914e06dc33f5b270b1e5c51936
Thomas <91213934+uvsq22104079@users.noreply.github.com>
2022-03-15 20:58:48 +0100

    Update cible.py

--- TD05_gui/cible.py
+++ TD05_gui/cible.py
@@ -6,10 +6,11 @@ root = tk.Tk()
 
 canvas = tk.Canvas(root, width = CANVAS_WIDTH, height = CANVAS_HEIGHT, bg = "#000000")
 
-x = 150
-y = 150
+x = 100
+y = 100
 
-canvas.create_oval( x, y, x+100, y+100, fill="blue", width=15)
+canvas.create_oval( x, y, x+300, y+300, fill="blue")
+canvas.create_oval( x, y, x+250, y+250, fill="green")
 
 canvas.grid()
 root.mainloop()
