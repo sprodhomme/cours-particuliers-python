ff145500d9094f1813464654df639169bd11bb80
Thomas <91213934+uvsq22104079@users.noreply.github.com>
2022-03-22 21:21:30 +0100

    Update balle(3).py

--- balle(3).py
+++ balle(3).py
@@ -5,7 +5,7 @@ import tkinter as tk
 
 LARGEUR = 600
 HAUTEUR = 400
-
+rebond_compte = 0
 
 ###################
 # Fonctions
@@ -32,11 +32,22 @@ def mouvement():
 def rebond():
     """Fait rebondir la balle sur les bords du canevas"""
     global balle
+    global rebond_compte
+    rectangle = canvas.create_rectangle(50, 50, 50, 50, fill="blue")
     x0, y0, x1, y1 = canvas.coords(balle[0])
     if x0 <= 0 or x1 >= 600:
         balle[1] = -balle[1]
+        rebond_compte = rebond_compte + 1
+        print(rebond_compte)
     if y0 <= 0 or y1 >= 400:
         balle[2] = -balle[2]
+        rebond_compte = rebond_compte + 1
+        print(rebond_compte)
+    if rebond_compte % 5 == 0:
+        canvas.itemconfigure(balle[0], fill="yellow")
+        
+    if rebond_compte % 10 == 0:
+        canvas.itemconfigure(balle[0], fill="blue")
 
 
 ######################
